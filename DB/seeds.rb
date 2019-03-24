0226423f2d0c96427b3a04cfb842bce76925494e
Alasdair Carstairs <alasdaircarstairs@Agnes-MBP.broadband>
2019-03-24 15:30:49 +0000

    model files and specs created - blank

---
+++ DB/seeds.rb
@@ -0,0 +1,13 @@
+require_relative('../Models/equipment.rb')
+require_relative('../Models/customer.rb')
+require_relative('../Models/lease.rb')
+
+# Equipment.delete_all
+# Customer.delete_all
+# Lease.delete_all
+
+#equipment----------------------------------------------------------------------
+
+#customers----------------------------------------------------------------------
+
+#leases-------------------------------------------------------------------------
